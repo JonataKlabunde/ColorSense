5e0fa607a6a704f2379719cf6ff086220758d734
Jonata Klabunde <jonata_k@yahoo.com.br>
2023-08-19 12:17:56 -0300

    init

---
+++ ColorSense/ColorSense/ColorSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSenseApp.swift
+//  ColorSense
+//
+//  Created by jonata klabunde on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
