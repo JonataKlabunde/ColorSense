2c128d6d99d94a2ca238af083b377c706b3767c6
Jonata Klabunde <jonata_k@yahoo.com.br>
2023-08-25 22:18:19 -0300

    voice recognizer

---
+++ ColorSense/ColorSense/Components/VoiceCommand.swift
@@ -0,0 +1,66 @@
+//
+//  VoiceCommand.swift
+//  ColorSense
+//
+//  Created by jonata klabunde on 23/08/23.
+//
+
+import SwiftUI
+import Speech
+import AVFoundation
+
+class VoiceCommand: ObservableObject {
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "pt-BR"))
+    private let audioEngine = AVAudioEngine()
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    @Published var isRecording = false
+    @Published var commandText: String = "Pronunciar um comando!"
+
+    
+    init() {
+        requestAuthorization()
+    }
+    
+    func requestAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { status in
+            switch status {
+            case .authorized:
+                print("Autorizado")
+            default:
+                print("Não autorizado")
+            }
+        }
+    }
+    
+    func start() throws {
+        isRecording = true
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Não foi possível criar o objeto SFSpeechAudioBufferRecognitionRequest.") }
+
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest) { result, error in
+            if let result = result {
+                self.commandText = result.bestTranscription.formattedString
+            } else if let error = error {
+                print("Houve um erro: \(error.localizedDescription)")
+            }
+        }
+        
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: audioEngine.inputNode.outputFormat(forBus: 0)) { buffer, _ in
+            self.recognitionRequest?.append(buffer)
+        }
+        audioEngine.prepare()
+        try audioEngine.start()
+    }
+    
+    func stop() {
+        isRecording = false
+        audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        recognitionRequest?.endAudio()
+        recognitionTask?.cancel()
+        recognitionTask = nil
+        recognitionRequest = nil
+    }
+}
