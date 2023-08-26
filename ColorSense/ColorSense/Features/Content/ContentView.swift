e57f045e282cba3401f2c294c7b20aa3e61e5242
Jonata Klabunde <jonata_k@yahoo.com.br>
2023-08-25 22:17:29 -0300

    content modifiers

--- ColorSense/ColorSense/Features/Content/ContentView.swift
+++ ColorSense/ColorSense/Features/Content/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject var viewModel = ContentViewModel()
+    @ObservedObject var voice = VoiceCommand()
+    @State private var timer: Timer?
+    @State private var microphoneEnable = false
+    @State private var scale: CGFloat = 1
+    private let animationTimer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {            
@@ -21,35 +26,83 @@ struct ContentView: View {
                     await viewModel.camera.start()
                 }
             
-            /// color name
-            colorName()
+            textBox()
         }
         .ignoresSafeArea()
         .background(Color.gray)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .gesture(DragGesture(minimumDistance: 0)
+            .onChanged { _ in
+                timer?.invalidate()
+                timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+                    if microphoneEnable == false {
+                        microphoneEnable = true
+                        viewModel.feedbackImpact(.rigid)
+                        /// long press                        
+                    }
+                }
+            }
+            .onEnded { _ in
+                timer?.invalidate()
+                if microphoneEnable {
+                    microphoneEnable = false
+                    /// long press finish
+                    viewModel.feedbackImpact(.rigid)
+                } else {
+                    /// touch
+                    viewModel.sayTheName()
+                }
+            }
+        )
     }
     
     @ViewBuilder
-    func colorName() -> some View {
+    func textBox() -> some View {
         VStack(spacing: 0) {
-            Spacer()
             VStack {
                 Text(viewModel.colorName)
                     .foregroundColor(.black)
                     .font(.system(size: 30, weight: .heavy))
+                    .padding(.vertical, 10)
             }
-            .frame(height: 100)
+            .frame(minHeight: 100)
             .frame(maxWidth: .infinity)
-//            .background(.ultraThinMaterial)
             .background(viewModel.color)
             .cornerRadius(20)
             .padding(.horizontal, 20)
-            .padding(.bottom, 60)
-            .onTapGesture {
-                viewModel.sayTheName()
-            }
+            .padding(.top, 60)
+            Spacer()
         }
     }
+    
+//    @ViewBuilder
+//    func microfoneIcon() -> some View {
+//        VStack {
+//            Spacer()
+//            Image(systemName: "mic.circle.fill")
+//                .resizable()
+//                .scaledToFill()
+//                .frame(width: 100, height: 100)
+//                .foregroundColor(.white.opacity(microphoneEnable ? 1.0 : 0.4))
+//                .animation(.easeInOut(duration: 0.3), value: microphoneEnable)
+////                .overlay {
+////                    if microphoneEnable {
+////                        Circle()
+////                            .stroke(.white.opacity(0.2), lineWidth: 2)
+////                            .scaleEffect(scale)
+////                            .onReceive(animationTimer) { _ in
+////                                withAnimation(Animation.linear(duration: 0.5).repeatForever(autoreverses: true)) {
+////                                    scale = scale == 1 ? 1.3 : 1
+////                                }
+////                            }
+////                    }
+////                }
+//                .padding(.bottom, 80)
+//        }
+//    }
+
+    
+    
     
 }
 
