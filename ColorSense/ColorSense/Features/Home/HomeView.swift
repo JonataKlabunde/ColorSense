39b1ceda73558e76f454fc7f04023eaafc78c7ec
Jonata Klabunde <jonata_k@yahoo.com.br>
2023-08-25 22:17:15 -0300

    home

---
+++ ColorSense/ColorSense/Features/Home/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  ColorSense
+//
+//  Created by jonata klabunde on 24/08/23.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct HomeView: View {
+    
+    @ObservedObject var viewModel = HomeViewModel()
+    
+    var body: some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .cornerRadius(30)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .sheet(isPresented: $viewModel.voicerIsVisible) {
+            SpeechView()
+                .presentationDetents([.height(200)])
+        }
+        .onTapGesture {
+            viewModel.voicerIsVisible = true
+        }
+        .onAppear {
+            viewModel.voicerIsVisible = true
+        }
+    }
+    
+}
---
+++ ColorSense/ColorSense/Features/Home/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  ColorSense
+//
+//  Created by jonata klabunde on 24/08/23.
+//
+
+import SwiftUI
+import Speech
+
+final class HomeViewModel: ObservableObject {
+    
+    @Published var voicerIsVisible: Bool = false
+
+}
